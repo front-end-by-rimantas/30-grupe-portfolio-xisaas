36871dbca9d7ea4b90e64b506a8b99a8a121709d
Vilija <86228325+Tamulaitiene@users.noreply.github.com>
2021-08-23 13:06:10 +0300

    Basic updates

--- js/home.js
+++ js/home.js
@@ -1,3 +1,27 @@
 // import JS modules
 
 // execute JS modules
+
+/* Header Area Start */
+/* Header Area End */
+
+/* Heros.. Area Start */
+/* Heros.. Area end */
+
+/* Icons Area Start */ 
+/* Icons Area End */
+
+/* Video Area Start */
+/* Video Area End */
+
+/* We focus on data ... area start */
+/* We focus on data ... area end */
+
+/* Frequently asked price area start */
+/* Frequently asked price area end */
+
+/* Need Contact.. area start */
+/* Need Contact..  area end */
+
+/* Be Notified area start */
+/* Be Notified area end */
