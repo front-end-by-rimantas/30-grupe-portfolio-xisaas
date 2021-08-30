9f95afadc291ca28e42f48c4551c6d55386e764f
Vilija <86228325+Tamulaitiene@users.noreply.github.com>
2021-08-30 11:57:01 +0300

    Bandymas

--- js/home.js
+++ js/home.js
@@ -1,3 +1,29 @@
 // import JS modules
 
 // execute JS modules
+
+/* Icons Area Start */
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        rensponsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 5
+            }
+        }]
+    });
+});
+
+/* Icons Area End */
